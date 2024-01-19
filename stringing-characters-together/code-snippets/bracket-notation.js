42b8d433a87c48f056b23f13c667ba2bc0736c4e
Ronnie Gentile <rgentile09@yahoo.com>
2024-01-18 22:09:56 -0600

    Array exercises complete

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -2,3 +2,10 @@ let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+
+let school = [
+    ["science", "computer", "art"],
+    ["Jones", "Willoughby", "Rhodes"]
+];
+school[1].unshift("Holmes");
+console.log(school);
