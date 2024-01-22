014ca37cd9d6434210d241a38421720f0134c456
Ronnie Gentile <rgentile09@yahoo.com>
2024-01-22 17:53:34 -0600

    Loop exercises complete

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,10 +3,21 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
-
-
-
-
+for (let i = 0; i < 21 ; i++ ) {
+  console.log(i);
+}
+for (let i = 3; i < 29; i++) {
+  if (i % 2 === 1) 
+    console.log(i);
+}
+for (let i = 12; i > -15; i--) {
+  if (i % 2 === 0)
+  console.log(i);
+}
+for (let i = 50; i >20; i--) {
+  if (i % 3 === 0)
+  console.log(i);
+}
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
 
@@ -14,6 +25,17 @@ Initialize two variables to hold the string “LaunchCode” and the array [1, 5
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+let launchOne = "LaunchCode";
+let launchTwo = [1, 5, 'LC101', 'blue', 42];
+for ( let i = 0; i < launchTwo.length; i++) {
+  console.log(launchTwo[i]);
+
+}
+let newLaunch = launchOne.split('').reverse();
+for (let i = 0; i < newLaunch.length; i++) {
+  
+  console.log(newLaunch[i]);
+} 
 
 
 
@@ -22,3 +44,25 @@ Construct ``for`` loops to accomplish the following tasks:
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+  let arr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evens = [];
+  let odds =[];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evens.push(arr[i]);
+      
+    } if (arr[i] % 2 ===1) {
+      odds.push(arr[i]);
+      
+    }
+    
+  
+  } 
+  console.log(evens);
+  console.log(odds);
+  let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,8 +1,16 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
+const input = require('readline-sync');
+let startingFuel = 0, numberAstronauts = -1, maxAltitude = 0;
 
 
 
+/*Exercise #4: Construct while loops to do the following:
+  a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
+  while (startingFuel < 5000 || startingFuel > 30000 || isNaN(startingFuel)) {
+    startingFuel = Number(input.question("How much fuel is the shuttle starting with? "))
+    
+  }
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
@@ -13,12 +21,23 @@
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
   
+  while (numberAstronauts >= 7 || numberAstronauts < 0 || isNaN(numberAstronauts)) {
+    numberAstronauts = Number(input.question("How many astronauts are on the shuttle? "));
+    
+  }
+    let i = 0;
+  
   
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
-
+  let numberOfTrips = 0
+while (startingFuel >= 0) {
+     startingFuel = startingFuel - (numberAstronauts * 100);
+    i = i + 1;
+   
+  }
+console.log(i * 50);
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
