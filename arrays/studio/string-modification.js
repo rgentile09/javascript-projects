886fcf9e172ab864f671ed0b87a766fcac65d61d
Ronnie Gentile <rgentile09@yahoo.com>
2024-02-04 21:13:56 -0600

    Array studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -2,10 +2,17 @@ const input = require('readline-sync');
 let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
+firstThree = str.slice(0,3);
+theRest = str.slice(3,11);
+pigLatin = `${theRest}${firstThree}`;
+console.log(`${str} reorganized with the first 3 characters at the end is ${pigLatin}!`);
+
+
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let howManyLetters = input.question("How many number of letters do you want to relocate? ");
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
