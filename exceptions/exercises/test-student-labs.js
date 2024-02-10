603e66ff4496c0a543a76795f63a48ec4584c31b
Ronnie Gentile <rgentile09@yahoo.com>
2024-02-10 13:39:53 -0600

    Exception exercises complete

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,12 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
+  try {
+     let lab = labs[i];
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
+  } catch(err) {
+    console.log(result = "Error Thrown")
+  }
   }
 }
 
@@ -20,5 +24,28 @@ let studentLabs = [
     }
   }
 ];
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
 
 gradeLabs(studentLabs);
